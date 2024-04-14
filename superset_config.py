f0149d5fe672dcbf46a8d236cb0e04bf155407b8
Ben Eberle <eberleben15@gmail.com>
2024-04-14 09:27:10 -0600

    adding config

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,3 @@
-import secrets
-
 # ---------------------------------------------------------
 # Superset specific config
 # ---------------------------------------------------------
@@ -11,7 +9,7 @@ ROW_LIMIT = 5000
 SUPERSET_WORKERS = 4
 
 # Set Secret Key
-SECRET_KEY = secrets.token_urlsafe(16)
+SECRET_KEY = 'ACpAhrXUZDkY1WH73qR0ZQ'
 
 # 
 SESSION_COOKIE_SAMESITE = None  # One of [None, 'Lax', 'Strict']
@@ -19,7 +17,11 @@ SESSION_COOKIE_SAMESITE = None  # One of [None, 'Lax', 'Strict']
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = False
 
+# Allow ADHOC Subqueries
+ALLOW_ADHOC_SUBQUERY=True
+
 # Branding
 LOGO_TARGET_PATH = 'https://sites.lsa.umich.edu/dcc-project/the-reckoning-project/'
 APP_NAME = 'The Reckoing Project'
+APP_LOGO = 'app/logo/TheReckoningProject_logo.png'
 WELCOME_MESSAGE = 'Welcome!'
